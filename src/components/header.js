d663463ce586606bc8ba0633773382efc8a525ac
ucolinmee <colinjmeehan23@gmail.com>
2024-05-24 11:17:57 +0800

    Change styling for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import Icon from '../images/mini.jpg';
+import Icon from '../images/header-logo.jpg';
 
 const loadHeader = () => {
     const header = document.querySelector('header');
@@ -9,21 +9,37 @@ const loadHeader = () => {
         'Contact'
     ]
 
-    const nav = document.createElement('nav');
+    const div = document.createElement('div');
 
-    // Add the image to navbar
     const myIcon = new Image();
     myIcon.src = Icon;
-    nav.appendChild(myIcon);
+    div.appendChild(myIcon);
+
+    const nav = document.createElement('nav');
 
     // Add each section to the navbar
     navSections.forEach((section) => {
         const button = document.createElement('button');
+        button.id = section.toLowerCase();
         button.innerHTML = section;
         nav.appendChild(button);
     })
 
+    header.appendChild(div);
     header.appendChild(nav);
 }
 
 export default loadHeader;
+
+{/* 
+<header>
+    <div>
+        <img src="" alt="">
+    </div>
+    <nav>
+        <button>Home</button>
+        <button>Menu</button>
+        <button>Contact</button>
+    </nav>
+</header> 
+*/}
--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,19 @@
 const loadHome = () => {
     const content = document.getElementById('content');
 
-    // Add image div
-    const imgDiv = document.createElement('div');
-    imgDiv.classList.add('bgImg');
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add('box');
 
     // Add headline
     const headline = document.createElement('h1');
     headline.innerHTML = "Welcome to Pekka's Pancakes";
+    tempDiv.appendChild(headline);
 
     const para = document.createElement('p');
     para.innerHTML = "Come find out what makes Pekka so obsessed over pancakes. Be it sweet, savoury or metallic... Pekka's Pancakes has something for your taste buds!"
+    tempDiv.appendChild(para);
 
-    content.appendChild(imgDiv);
-    content.appendChild(headline);
-    content.appendChild(para);
+    content.appendChild(tempDiv);
 }
 
 export default loadHome;
