a443aad7158806410666841f4346b7eda262cdcd
ucolinmee <colinjmeehan23@gmail.com>
2024-05-27 15:42:33 +0800

    Add a social section to the home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,53 @@
 import Icon from '../images/home.png';
+import TikTok from '../images/icons/tiktok.svg';
+import Instagram from '../images/icons/instagram.png';
+import Twitter from '../images/icons/twitter.png'; 
 
 const loadHome = () => {
     const content = document.getElementById('content');
 
-    const div = document.createElement('div');
-    div.id = 'home';
+    // *** <div class="home"> ***
+    const homeDiv = document.createElement('div');
+    homeDiv.classList.add('home');
 
     // img
     const myIcon = new Image();
     myIcon.src = Icon;
-    div.appendChild(myIcon);
+    homeDiv.appendChild(myIcon);
 
     // h1
     const headline = document.createElement('h1');
     headline.innerHTML = "Welcome to Pekka's Pancakes";
-    div.appendChild(headline);
+    homeDiv.appendChild(headline);
 
     // p
     const para = document.createElement('p');
     para.innerHTML = "Come find out what makes Pekka so obsessed over pancakes. Be it sweet, savoury or metallic... Pekka's Pancakes has something for your taste buds! "
-    div.appendChild(para);
+    homeDiv.appendChild(para);
 
-    content.appendChild(div);
+    // *** <div class="socials"> ***
+    const socialsDiv = document.createElement('div');
+    socialsDiv.classList.add('socials');
+
+    // h1
+    const findUs = document.createElement('h1');
+    findUs.innerHTML = "Find Us";
+    socialsDiv.appendChild(findUs);
+
+    // <div>
+    const faviconsDiv = document.createElement('div');
+
+    const faviconsList = [Instagram, Twitter, TikTok];
+
+    faviconsList.forEach((icon) => {
+        const social = document.createElement('img');
+        social.src = icon;
+        faviconsDiv.appendChild(social);
+    })
+    socialsDiv.appendChild(faviconsDiv);
+
+    content.appendChild(homeDiv);
+    content.appendChild(socialsDiv);
 }
 
 export default loadHome;
@@ -29,10 +55,18 @@ export default loadHome;
 
 {/* 
 <div id="content">
-    <div id="home">
+    <div class="home">
         <img src="" alt="">
         <h1>Welcome to Pekka's Pancakes</h1>
         <p>Come find out what makes Pekka so obsessed...</p>
+    </div>
+    <div class="socials">
+        <h1>Find us</h1>
+        <div>
+            <img>Instagram
+            <img>Twitter
+            <img>Tiktok
+        </div>
     </div>
 </div> 
 */}
